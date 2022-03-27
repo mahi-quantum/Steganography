f1612b112eea9fcdf16f3fcbacafd0808bd841c1
time-whispers <102413182+time-whispers@users.noreply.github.com>
2022-03-27 13:06:19 +0530

    1st Commit
    My 1st Commit

---
+++ Steganography.py
@@ -0,0 +1,81 @@
+from tkinter import*
+from tkinter import filedialog
+import tkinter as tk
+import PIL
+from PIL import ImageTk
+from PIL import Image
+import os
+from stegano import lsb
+
+root = Tk()
+root.title("Steganography - Hide a Secret Text Message in an Image")
+root.geometry("700x500+250+180")
+root.resizable(False,False)
+root.configure(bg="#2f4155")
+
+def showimage():
+    global filename
+    filename=filedialog.askopenfilename(initialdir=os.getcwd(), title='Select Image File', filetype=(("PNG file","*.png"), ("JPG file","*.jpg"), ("All files","*.txt")))
+    img=Image.open(filename)
+    img=ImageTk.PhotoImage(img)
+    lbl.configure(image=img,width=250,height=250)
+    lbl.image=img
+
+def hidedata():
+    global secret
+    message=text1.get(1.0,END)
+    secret = lsb.hide(str(filename), message)
+
+def saveimage():
+    secret.save("hidden.png")
+
+def showdata():
+    clear_message = lsb.reveal(filename)
+    text1.delete(1.0, END)
+    text1.insert(END, clear_message)
+
+#icon
+image_icon=PhotoImage(file="logo.jpg")
+root.iconphoto(False,image_icon)
+
+#logo
+logo=PhotoImage(file="logo.png")
+Label(root,image=logo,bg="#2f4155").place(x=10,y=0)
+Label(root,text="Crytography",bg="#2d4155",fg="white",font="arial 25 bold").place(x=100,y=20)
+
+#First Frame
+f1=Frame(root,bd=3,bg="black",width=340,height=280,relief=GROOVE)
+f1.place(x=10,y=80)
+
+lbl=Label(f1,bg="black")
+lbl.place(x=40,y=10)
+
+#Second Frame
+f2=Frame(root,bd=3,bg="white",width=340,height=280,relief=GROOVE)
+f2.place(x=350,y=80)
+
+text1=Text(f2,font="Robote 20",bg="white",fg="black",relief=GROOVE)
+text1.place(x=0,y=0,width=320,height=295)
+
+scrollbar1=Scrollbar(f2)
+scrollbar1.place(x=320,y=0,height=300)
+scrollbar1.configure(command=text1.yview)
+text1.configure(yscrollcommand=scrollbar1.set)
+
+#Third Frame
+f3=Frame(root,bd=3,bg="#2f4155",width=330,height=100,relief=GROOVE)
+f3.place(x=10,y=370)
+
+Button(f3,text="Open Image",width=10,height=2,font="arial 14 bold",command=showimage).place(x=20,y=30)
+Button(f3,text="Save Image",width=10,height=2,font="arial 14 bold",command=saveimage).place(x=180,y=30)
+Label(f3,text="Image, Pics, All Files", bg="#2f4155",fg="yellow").place(x=20,y=5)
+
+#Fourth Frame
+f4=Frame(root,bd=3,bg="#2f4155",width=330,height=100,relief=GROOVE)
+f4.place(x=360,y=370)
+
+Button(f4,text="Hide Data",width=10,height=2,font="arial 14 bold",command=hidedata).place(x=20,y=30)
+Button(f4,text="Show Data",width=10,height=2,font="arial 14 bold",command=showdata).place(x=180,y=30)
+Label(f4,text="Steganography", bg="#2f4155",fg="yellow").place(x=20,y=5)
+
+root.mainloop()
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Python Steganography
+
+Steganography is the art of concealing a secret message within (or even on top of) a non-secret object. That something can be anything you want it to be. Many forms of steganography nowadays involve hiding a secret piece of text within a photograph. Alternatively, you may hide a secret message or script inside a Word or Excel document. 
+
+Steganography's goal is to conceal and deceive. It is a type of covert communication in which messages are hidden using any media. It isn't cryptography because it doesn't encrypt data or require the usage of a key. Instead, it's a type of data concealment that can be done in a variety of ways. Steganography is a practise that enables secrecy – and deception – in the same way that cryptography is a science that mostly enables privacy. 
+
+## Instructions for Use:
+
+- Clone the repository or download the contents of the repository as a Zip file.
+- Set up Python on your computer
+- Run the following command in your terminal to install the python packages listed in the requirments.txt file:
+
+    ```python3
+    >>> pip install -r requirements.txt #for windows
+    >>> pip3 install -r requirements.txt #for macos and linux
+    ```
+ 
+- Run the Steganography.py file by running <i>python Steganography.py</i> or <i>Steganography.py</i> on it or simply double click the Steganography.py program
+- Now, go to the Open Image option and choose the image you wish to share, then type the text you want to send as a hidden message.
+- After that, click Hide data and then Save Image, which will then be saved as a hidden.png file.
+- Simply share this file with anyone you choose, and only he or she will be able to see the hidden message.
